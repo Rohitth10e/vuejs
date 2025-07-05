3a0176d44cd57c12565f44156dcbcfe49caf9600
rohith10e <rohith10e@outlook.com>
2025-07-06 00:35:02 +0530

    feat: exploring vuex:getters & actions

--- gswvx/src/App.vue
+++ gswvx/src/App.vue
@@ -1,39 +1,58 @@
 <script setup>
-import { computed } from "vue";
+import { computed,onMounted } from "vue";
 import { useStore } from "vuex";
 
 const store = useStore();
 
 const count = computed(() => store.state.count);
-const isLiked = computed(()=>store.state.isLiked);
-const isReset = computed(()=>store.state.isReset);
+const isLiked = computed(() => store.state.isLiked);
+const isReset = computed(() => store.state.isReset);
+const message = computed(() => store.getters.statusMessage);
+const doubleCount = computed(() => store.getters.doubleCount);
+const greetingMessage = computed(() => store.getters.greetingMessage);
+const isAdult = computed(() => store.getters.isAdult);
+
+const name = computed(()=>store.state.user.name);
+
+onMounted(()=>{
+  store.dispatch('fetchUser')
+})
+
 
 const like = () => {
-  store.commit("like")
-}
+  store.commit("like");
+};
 
 const reset = () => {
-  store.commit("reset")
-}
-
+  store.commit("reset");
+};
 </script>
 
 <template>
-  <p class="count-txt">{{ count }}</p>
-
   <div>
-    <p v-if="isLiked">User liked</p>
-    <p v-else>User disliked</p>
-    <button @click="like">
-      {{ isLiked ? 'unlike' : 'like' }}
-    </button>
-    <button @click="reset">reset</button>
-  </div>
+    <p class="count-txt">{{ count }}</p>
+    <p>{{ message }}</p>
+    <p>{{ doubleCount }}</p>
+    <p>{{ greetingMessage }}</p>
+    <p>{{ isAdult }}</p>
+    <div>
+      <p v-if="isLiked">User liked</p>
+      <p v-else>User disliked</p>
+      <button @click="like">
+        {{ isLiked ? "unlike" : "like" }}
+      </button>
+      <button @click="reset">reset</button>
+    </div>
 
+    <div>
+      <h1>Hello {{ name }}</h1>
+    </div>
+
+  </div>
 </template>
 
 <style scoped>
-.count-txt{
+.count-txt {
   color: white;
 }
 </style>
--- gswvx/src/store/index.js
+++ gswvx/src/store/index.js
@@ -1,33 +1,67 @@
-import { createStore } from 'vuex'
+import axios from "axios";
+import { createStore } from "vuex";
 
 const store = createStore({
-    state(){
-        return{
-            count:0,
-            isLiked:true,
-            isReset:false
-        }
+  state() {
+    return {
+      count: 0,
+      isLiked: true,
+      isReset: false,
+      user: {
+        name: "",
+        age: null,
+      },
+    };
+  },
+  mutations: {
+    increment(state) {
+      state.count++;
     },
-    mutations:{
-        increment(state){
-            state.count++
-        },
-        decrement(state){
-            state.count--
-        },
-        like(state){
-            state.isLiked = !state.isLiked;
-            if(state.isLiked){
-                state.count++
-            }else{
-                state.count--
-            }
-        },
-        reset(state){
-            state.isReset = !state.isReset
-            state.count = 0
-        }
-    }
-})
+    decrement(state) {
+      state.count--;
+    },
+    like(state) {
+      state.isLiked = !state.isLiked;
+      if (state.isLiked) {
+        state.count++;
+      } else {
+        state.count--;
+      }
+    },
+    reset(state) {
+      state.isReset = !state.isReset;
+      state.count = 0;
+    },
+    setUser(state, name) {
+      state.user.name = name;
+    },
+  },
+  getters: {
+    statusMessage(state) {
+      return state.isLiked ? "You liked the post" : "You unliked the post";
+    },
+    doubleCount(state) {
+      return state.count * state.count;
+    },
+    greetingMessage(state) {
+      return `Hello ${state.user.name}, You are ${state.user.age} old`;
+    },
+    isAdult(state) {
+      return state.user.age < 18 ? `Minor` : `Adult`;
+    },
+  },
+  actions: {
+    async fetchUser({ commit }) {
+      try {
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/users/1"
+        );
+        commit("setUser", response.data.name);
+      } catch (err) {
+        console.error('error fetching data: ',err.message)
+      }
+    },
+  },
+});
 
 export default store;
